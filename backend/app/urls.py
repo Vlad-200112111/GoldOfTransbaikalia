a8bb55fc40e77c5383a75e7ea202cd37f05f9097
Vlasov Viktor <vvlasovdo@mail.ru>
2022-10-25 17:13:00 +0900

    comments

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("news/create/", NewsCreateAPIView.as_view()),
     path("news/", NewsListAPIView.as_view()),
     path("news/<int:pk>", NewsDetailAPIView.as_view()),
-    path("comments/", CommentsListAPIView.as_view()),
-    path("comments/create/", CommentsCreateAPIView.as_view()),
-    path("comments/<int:pk>", CommentsDetailAPIView.as_view()),
+    path("news/<int:pk>/comments/", CommentsListAPIView.as_view()),
+    path("news/comments/create/", CommentsCreateAPIView.as_view()),
 ]
